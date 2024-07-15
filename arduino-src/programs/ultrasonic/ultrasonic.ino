57bbe154c4f085338d6ce1b3d818c8cd9f817e05
JakeHamblin <jake@jakehamblin.com>
2024-07-15 16:10:56 -0400

    Created programs (most work)

---
+++ arduino-src/programs/ultrasonic/ultrasonic.ino
@@ -0,0 +1,51 @@
+const int trigPin = 12;
+const int echoPin = 13;
+
+void setup() {
+  // Start serial output
+  Serial.begin(9600);
+
+  // Set trigger pin to output
+  pinMode(trigPin, OUTPUT);
+
+  // Set echo pin as input
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  // Distance of object from sensor
+  float distance;
+
+  // Get distance of object from sensor
+  distance = showDistance();
+
+  Serial.println(distance);
+
+  // Print line for pretty console
+  Serial.println();
+  delay(25);
+}
+
+float showDistance() {
+  // Get duration and distance
+  float duration, distance;
+
+  // Stop sending any trigger
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Send trigger
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+
+  // Stop sending any trigger
+  digitalWrite(trigPin, LOW);
+
+  // Calculate distance away from sensor
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  delay(25);
+
+  // Return distance
+  return distance;
+}
